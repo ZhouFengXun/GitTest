11d64747ab4abcac919400130e3a4b013427976f
zhoufengxun <2281461277@qq.com>
2022-09-26 17:10:24 +0800

    整理接口

--- demo/src/main/java/com/example/demo/controller/userController.java
+++ demo/src/main/java/com/example/demo/controller/userController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
