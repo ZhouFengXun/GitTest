f20cf4deff4724301763eb0b7016acaefc54abda
zhoufengxun <2281461277@qq.com>
2022-09-26 17:16:14 +0800

    提交本地仓库

--- demo/src/main/java/com/example/demo/controller/userController.java
+++ demo/src/main/java/com/example/demo/controller/userController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class userController {
+
+    @PostMapping
+    public void show(){
+        System.out.println("展示");
+    }
 
 }
