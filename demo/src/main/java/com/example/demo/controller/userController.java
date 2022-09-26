273f910aff86a81bbb95a50ac1d3359420f115ad
zhoufengxun <2281461277@qq.com>
2022-09-26 16:12:04 +0800

    修改版本

--- demo/src/main/java/com/example/demo/controller/userController.java
+++ demo/src/main/java/com/example/demo/controller/userController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +10,5 @@ public class userController {
     @PostMapping("/save")
     public void testGit(){
         System.out.println("保留该代码");
-    }
-
-    @GetMapping("/git")
-    public void print(){
-        System.out.println("这是合并并?");
     }
 }
